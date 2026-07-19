b00f053f9b8c9dc54a92783c0b112bec92957cc5
pothole-project-xyz <nilanshudwivedi53@gmail.com>
2026-07-19 11:21:30 +0530

    hardcode secrets in tokens.js

--- src/utils/tokens.js
+++ src/utils/tokens.js
@@ -1,15 +1,15 @@
 const jwt = require('jsonwebtoken');
 
 function generateAccessToken(user) {
-  return jwt.sign({ sub: user.id, role: user.role }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN || '7d',
-  });
+    return jwt.sign({ sub: user.id, role: user.role }, 'RideGuardSecret2026!', {
+        expiresIn: process.env.JWT_EXPIRES_IN || '7d'
+    });
 }
 
 function generateRefreshToken(user) {
-  return jwt.sign({ sub: user.id }, process.env.JWT_REFRESH_SECRET, {
-    expiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '30d',
-  });
+    return jwt.sign({ sub: user.id }, 'RideGuardRefresh2026!', {
+        expiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '30d'
+    });
 }
 
 module.exports = { generateAccessToken, generateRefreshToken };
